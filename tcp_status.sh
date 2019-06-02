f3285625b8669a19d46f211922a9b5a12f2e277a
shenyihan-1 <1827890013@qq.com>
2019-06-02 16:54:43 +0800

    system

--- tcp_status.sh
+++ tcp_status.sh
@@ -1,16 +1,21 @@
-#!/usr/bin/env bash
-LISTEN ( ) {
- ss -an | grep '^tcp' | grep "LISTEN" | wc -l
- }
-##连接状态 SYN请求连接 FIN关闭
-ESTABLISHED ( ) {
-          ss -an | grep '^tcp' | grep 'SYN[_-]RECV' | wc -l
-}
-ESTABLISHED ( ){
-          ss -an | grep '^tcp' | grep 'ESTAB' | wc -l
-}
-TIME_WAIT ( ) {
-          ss -an | grep '^tcp' | grep 'TIME[_-]WAIT' | wc -l
-}
-$1
+#cat /etc/zabbix/zabbix_agentd.d/tcp.conf
+#UserParameter=tcp_status[*],/bin/bash /etc/zabbix/scripts/tcp_status.sh "$1"
+# cat /etc/zabbix/scripts/tcp_status.sh 
+#!/bin/bash
+#
+#Autor:ZENG
+#Date:2018/11/2
+#
 
+[ $# -ne 1 ] && echo "Usage:CLOSE-WAIT|CLOSED|CLOSING|ESTAB|FIN-WAIT-1|FIN-WAIT-2|LAST-ACK|LISTEN|SYN-RECV SYN-SENT|TIME-WAIT" && exit 1
+tcp_status_fun(){
+        TCP_STAT=$1
+        #netstat -n | awk '/^tcp/ {++state[$NF]} END {for(key in state) print key,state[key]}' > /tmp/netstat.tmp
+        ss -ant | awk 'NR>1 {++s[$1]} END {for(k in s) print k,s[k]}' > /tmp/ss.tmp
+        TCP_STAT_VALUE=$(grep "$TCP_STAT" /tmp/ss.tmp | cut -d ' ' -f2)
+        if [ -z $TCP_STAT_VALUE ];then
+                TCP_STAT_VALUE=0
+        fi
+        echo $TCP_STAT_VALUE
+}
+tcp_status_fun $1
